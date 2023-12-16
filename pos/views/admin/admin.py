a9935c69d529ff71a59be22cc8b2b29237552264
VTran09 <98932527+VTran09@users.noreply.github.com>
2023-12-16 00:31:03 -0500

    Delete pos directory

--- pos/views/admin/admin.py
+++
@@ -1,9 +0,0 @@
-
-from kivy.uix.boxlayout import BoxLayout
-from kivy.clock import Clock, mainthread
-from kivy.lang import Builder
-
-Builder.load_file("views/admin/admin.kv")
-class Admin(BoxLayout):
-    def __init__(self, **kw) -> None:
-        super().__init__(**kw)
